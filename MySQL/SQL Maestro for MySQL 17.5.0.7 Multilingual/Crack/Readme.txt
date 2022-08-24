64d573be9c772253f1735b386f9c8ce64b90772f
luca lopez <101877092+xluca15@users.noreply.github.com>
2022-08-24 08:44:40 -0300

    Add files via upload

---
+++ MySQL/SQL Maestro for MySQL 17.5.0.7 Multilingual/Crack/Readme.txt
@@ -0,0 +1,3 @@
+Copy & replace patched file to installation folder
+=================
+www.downloadly.ir
---
+++ MySQL/articulos.sql
@@ -0,0 +1,11 @@
+INSERT INTO articulos (idarticulo, nombrearticulo, precioarticulo, idfabricante) VALUES
+  (10, 'Tarjeta de red', 1800, 3),
+  (9, 'CD Rom', 2000, 2),
+  (8, 'DVD Rom', 4500, 2),
+  (7, 'Memoria USB', 2790, 1),
+  (6, 'Disco duro extraible 800 Gb', 6500, 5),
+  (5, 'Memoria RAM', 2900, 1),
+  (4, 'Memoria USB', 1400, 4),
+  (3, 'Mouse', 800, 3),
+  (2, 'Hdd 730 Gb', 5000, 5),
+  (1, 'Teclado', 1000, 3);
