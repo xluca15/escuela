1e6fecc979e6e18fafbac901b318756d0294d3b5
luca lopez <101877092+xluca15@users.noreply.github.com>
2024-06-27 18:48:46 -0300

    Delete 7/PDIS/Js8 directory

--- 7/PDIS/Js8/Js9/ejercicio 1/main.js
+++
@@ -1,6 +0,0 @@
-// Importamos las funciones desde math.js
-import { sumar, restar } from './math.js';
-
-// Usamos las funciones importadas
-console.log("Suma: 5 + 3 =", sumar(5, 3));
-console.log("Resta: 10 - 4 =", restar(10, 4));
--- 7/PDIS/Js8/Js9/ejercicio 1/math.js
+++
@@ -1,9 +0,0 @@
-// Función para sumar
-export function sumar(a, b) {
-    return a + b;
-}
-
-// Función para restar
-export function restar(a, b) {
-    return a - b;
-}
